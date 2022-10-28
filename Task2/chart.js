f73914c0666d1bfc3cadcaf93a30efd90cb524da
akshaayan <94971860+akshaayan@users.noreply.github.com>
2022-10-28 15:14:04 +0600

    Update chart.js

--- Task2/chart.js
+++ Task2/chart.js
@@ -1,70 +1,130 @@
-// set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 70, left: 60},
-    width = 1000 - margin.left - margin.right,
-    height = 1000 - margin.top - margin.bottom;
+async function loadDataset() {
+    data = await d3.json("./my_weather_data.json")
+}
 
-// append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+const tempLowAcc = d => d.temperatureLow;
+const tempHighAcc = d => d.temperatureHigh;
+const tempMinAcc = d => d.temperatureMin;
+const tempMaxAcc = d => d.temperatureMax;
 
-// Initialize the X axis
-var x = d3.scaleBand()
-    .range([ 0, width ])
-    .padding(0.2);
-var xAxis = svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
+function clearBox(elementID)
+{
+    document.getElementById(elementID).innerHTML = "";
+}
 
-// Initialize the Y axis
-var y = d3.scaleLinear()
-    .range([ height, 0]);
-var yAxis = svg.append("g")
-    .attr("class", "myYaxis")
+function initiate(tempAccessor) {
+    //Accessor
+    clearBox("histogram")
+    var yAccessor = d => d.length;
+
+    const width = document.getElementById("histogram").clientWidth;
+    let dimensions = {
+        width: width,
+        height: width * 0.6,
+        margin: {
+            left: 50,
+            right: 50,
+            top: 30,
+            bottom: 30,
+        },
+    }
+
+    dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right;
+    dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+    // 3. Draw canvas
+
+    const wrapper = d3.select("#histogram").append("svg")
+        .attr("width", dimensions.width)
+        .attr("height", dimensions.height);
+
+    const bounds = wrapper.append("g")
+        .style("translate",`translate(${dimensions.margin.left}px,${dimensions.margin.top}px)`);
+
+    const xScaler = d3.scaleLinear()
+        .domain(d3.extent(data,tempAccessor))
+        .range([dimensions.margin.left, dimensions.width - dimensions.margin.right])
+
+    const binsGen = d3.bin()
+        .domain(xScaler.domain())
+        .value(tempAccessor)
+        .thresholds(20);
+
+    const bins = binsGen(data);
+
+    const yScaler = d3.scaleLinear()
+        .domain([0, d3.max(bins, yAccessor)])
+        .range([dimensions.height - dimensions.margin.bottom - dimensions.margin.top, dimensions.margin.top])
+
+    const binGroup = bounds.append("g");
+    const binGroups = binGroup.selectAll("g")
+        .data(bins)
+        .enter()
+        .append("g");
 
 
-// A function that create / update the plot for a given variable:
-function update(selectedVar) {
+    const barPadding = 5
+    const barRect = binGroups.append("rect")
+        .attr("x", d => xScaler(d.x0) + barPadding/2)
+        .attr("y", d => yScaler(yAccessor(d)))
+        .attr("width", d => d3.max([0, xScaler(d.x1) - xScaler(d.x0) - barPadding]))
+        .attr("height", d => dimensions.boundedHeight - yScaler(yAccessor(d)))
+        .attr("fill", "#FFAC33");
 
+    const mean = d3.mean(data,tempAccessor)
+    const meanLine = bounds.append("line")
+        .attr("x1", xScaler(mean))
+        .attr("x2", xScaler(mean))
+        .attr("y1", -15)
+        .attr("y2", dimensions.boundedHeight)
+        .attr("stroke","red")
+        .attr("stroke-dasharray","2px 4px");
 
-    //console.table(data);
-    const dateParser = d3.timeParse("%Y-%m-%d");
+    const meanLabel = bounds.append("text")
+        .attr("x",xScaler(mean))
+        .attr("y",10)
+        .text("Mean Point")
+        .attr("font-size","12px")
+        .attr("text-anchor","middle");
 
-    // Parse the Data
-    d3.json("my_weather_data.json", function(data) {
+    const xAxisGen = d3.axisBottom()
+        .scale(xScaler);
+    const xAxis = bounds.append("g")
+        .call(xAxisGen)
+        .style("transform",`translateY(${dimensions.boundedHeight}px)`);
 
-        // X axis
-        x.domain(data.map(function(d) { return d[selectedVar]; }))
-        xAxis.transition().duration(10).call(d3.axisBottom(x))
+    const yAxisGen = d3.axisLeft()
+        .scale(yScaler)
+    const yAxis = bounds.append("g")
+        .call(yAxisGen)
 
-        // Add Y axis
-        y.domain([0, d3.max(data, function(d) { return dateParser(d.date); }) ]);
-        yAxis.transition().duration(10).call(d3.axisLeft(y));
-
-        // variable u: map data to existing bars
-        var u = svg.selectAll("rect")
-            .data(data)
-
-        // update bars
-        u
-            .enter()
-            .append("rect")
-            .merge(u)
-            .transition()
-            .duration(10)
-            .attr("x", function(d) { return x(d[selectedVar]); })
-            .attr("y", function(d) { return y(dateParser(d.date)); })
-            .attr("width", x.bandwidth())
-            .attr("height", function(d) { return height - y(dateParser(d.date)); })
-            .attr("fill", "#69b3a2")
-    })
+    const barText = binGroups.filter(yAccessor)
+        .append("text")
+        .attr("x", d => xScaler(d.x0) + (xScaler(d.x1)-xScaler(d.x0))/2)
+        .attr("y", d => yScaler(yAccessor(d)) - 10)
+        .text(yAccessor)
+        .attr("fill","blue")
+        .attr("font-size","10px")
+        .attr("text-anchor","middle");
 
 }
 
-// Initialize plot
-update('var1')
 
+function TempLow() {
+    initiate(tempLowAcc);
+}
+function TempHigh() {
+    initiate(tempHighAcc);
+}
+function TempMin() {
+    initiate(tempMinAcc);
+}
+function TempMax() {
+    initiate(tempMaxAcc);
+}
 
+async function run(){
+    await loadDataset();
+    initiate(tempLowAcc);
+}
+
+run()
